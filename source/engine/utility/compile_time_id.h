310d4487618588ca25c573ced268a8749a091663
kdavison <kdavison@gmail.com>
2019-12-28 15:11:33 -0700

    update!

--- source/engine/utility/compile_time_id.h
+++ source/engine/utility/compile_time_id.h
@@ -1,15 +1,18 @@
 #pragma once
 
+#include <cstdint>
+
 namespace compile {
 template<typename T>
-static constexpr std::size_t id() {
-  return reinterpret_cast<std::size_t>(&id<T>);
+static constexpr std::size_t identifier() {
+  return reinterpret_cast<std::size_t>(&identifier<T>);
 }
 
 template<typename T>
-struct identifier {
-  static constexpr std::size_t ID() {
-    return id<T>();
+struct type {
+  std::size_t id() {
+    return ::compile::identifier<T>();
   }
 };
+
 } //::compile
